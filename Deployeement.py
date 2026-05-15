3e87be945cb02c84abb63da84d0364c806a382c2
unknown <mhdamaann@gmail.com>
2026-05-15 22:41:21 +0530

    Fix syntax error in Deployeement.py

--- Deployeement.py
+++ Deployeement.py
@@ -13,7 +13,7 @@ chest_pain_type     = chest_pain_type = st.selectbox("Chest Pain Type",options={
                                                                                 "2 – Atypical Angina": 2,
                                                                                 "3 – Non-Anginal Pain": 3,
                                                                                 "4 – Asymptomatic": 4,}.keys(),
-                      help="Type of chest pain experienced"
+                      help="Type of chest pain experienced")
 resting_bp          = st.number_input("Resting Blood Pressure (mm Hg)",min_value=50,max_value=260,value=0,step=1
 cholesterol         = st.number_input("Serum Cholesterol (mg/dl)",min_value=70,max_value=600,value=0,step=1)
 fasting_blood_sugar = st.selectbox("Fasting Blood Sugar > 120 mg/dl",options=["Yes", "No"],
